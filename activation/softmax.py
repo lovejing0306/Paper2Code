1f54d6570596f18951a2a2978c4eaf36dd39e70a
lovejing0306 <lovejing0306@gmail.com>
2025-11-25 13:38:06 +0800

    update paper2code

--- activation/softmax.py
+++ activation/softmax.py
@@ -6,7 +6,8 @@ def softmax(z):
     """Softmax 函数（用于输出层）"""
     max_value = np.max(z, axis=-1, keepdims=True)  # 求最大值
     exp_z = np.exp(z - max_value)  # 先对每个元素减去最大值，目的为了数值稳定性
-    return exp_z / np.sum(exp_z, axis=-1, keepdims=True)
+    out = exp_z / np.sum(exp_z, axis=-1, keepdims=True)
+    return out
 
 
 if __name__ == '__main__':
